5a14fea3b3ee5da3f6939f6b12810731782792d2
luisdt <luis.ernesto951008@gmail.com>
2020-03-02 09:01:31 -0300

    Add some utils

--- utils.py
+++ utils.py
@@ -1,12 +1,32 @@
 from random import randint
 
 
-def generate_random_frame(w, h):
+def generate_random_frame(w, h, pix):
     result = []
-    for x in range(h):
+    pixel_count = 0
+    for _ in range(h):
         row = []
-        for y in range(w):
-            row.append(randint(0, 1))
+        for _ in range(w):
+            if pixel_count < pix:
+                rand_pix = randint(0, 1)
+                row.append(rand_pix)
+                pixel_count += rand_pix
+            else:
+                row.append(0)
+        result.append(row)
+
+    return result
+
+
+def generate_x(w, h):
+    result = []
+    for x in range(h + 1):
+        row = []
+        for y in range(w + 1):
+            if x == y / 2 or y == y + 1 - x * 2:
+                row.append(1)
+            else:
+                row.append(0)
         result.append(row)
 
     return result
